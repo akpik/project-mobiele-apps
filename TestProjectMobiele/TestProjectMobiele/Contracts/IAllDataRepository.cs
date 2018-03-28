e9683736d9210c2e31a19bbf19d4847a1216ed2b
Kaan Akpinar <kaan.akpinar@hotmail.com>
2018-03-28 20:36:36 +0200

    Basis structuur aangemaakt. Paar klasses zodat duidelijk wordt hoe programma gaat werken

---
+++ TestProjectMobiele/TestProjectMobiele/Contracts/IAllDataRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProjectMobiele.Contracts
+{
+    public interface IAllDataRepository
+    {
+        
+    }
+}
---
+++ TestProjectMobiele/TestProjectMobiele/Models/Kleuter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProjectMobiele.Models
+{
+    public class Kleuter
+    {
+    }
+}
