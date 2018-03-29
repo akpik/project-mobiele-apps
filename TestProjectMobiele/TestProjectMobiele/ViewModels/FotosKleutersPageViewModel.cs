0f12ddcffd6323f8f88a3c50c2306d5f56c0c005
Kaan Akpinar <kaan.akpinar@hotmail.com>
2018-03-29 17:00:35 +0200

    Foto trekken proberen na kiezen kleuter

--- TestProjectMobiele/TestProjectMobiele/ViewModels/FotosKleutersPageViewModel.cs
+++ TestProjectMobiele/TestProjectMobiele/ViewModels/FotosKleutersPageViewModel.cs
@@ -1,16 +1,58 @@
-﻿using Prism.Commands;
+﻿using Plugin.Media;
+using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace TestProjectMobiele.ViewModels
 {
-	public class FotosKleutersPageViewModel : BindableBase
+	public class FotosKleutersPageViewModel : ViewModelBase
 	{
-        public FotosKleutersPageViewModel()
+        public ICommand ImageKleuterClicked { get; private set; }
+        private IPageDialogService dialogService;
+        public FotosKleutersPageViewModel(INavigationService navigationService, IPageDialogService dialogService) : base(navigationService)
         {
-
+            ImageKleuterClicked = new DelegateCommand(ExecuteTakePhotoCommand);
+            this.dialogService = dialogService;
         }
-	}
+
+        private ImageSource source;
+        public ImageSource Source
+        {
+            get { return source; }
+            set { SetProperty(ref source, value); }
+        }
+        private async void ExecuteTakePhotoCommand()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await dialogService.DisplayAlertAsync("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            await dialogService.DisplayAlertAsync("File Location", file.Path, "OK");
+
+            Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });
+        }
+    }
 }
