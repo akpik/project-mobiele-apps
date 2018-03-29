2337dcc0e138b57941b5f9a7dd08d8b8b399f832
Kaan Akpinar <kaan.akpinar@hotmail.com>
2018-03-29 15:51:56 +0200

    Specific data interface aangemaakt

--- TestProjectMobiele/TestProjectMobiele/Contracts/IAllDataRepository.cs
+++ TestProjectMobiele/TestProjectMobiele/Contracts/IAllDataRepository.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using TestProjectMobiele.Models;
 
 namespace TestProjectMobiele.Contracts
 {
     public interface IAllDataRepository
     {
+        Task<List<Kleuter>> GetKleutersAsync(string klascode);
+
+
         
     }
 }
---
+++ TestProjectMobiele/TestProjectMobiele/Models/Klas.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProjectMobiele.Models
+{
+    class Klas
+    {
+        public string Klascode { get; set; }
+    }
+}
